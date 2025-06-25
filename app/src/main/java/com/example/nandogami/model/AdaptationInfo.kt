4cbff6e27124f13469fb4bfc777927c420597368
anndaanhr <151341974+anndaanhr@users.noreply.github.com>
2025-06-25 15:33:05 +0700

    a

--- app/src/main/java/com/example/nandogami/model/AdaptationInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.nandogami.model
-
-data class AdaptationInfo(
-    val studio: String = "",
-    val title: String = "",
-    val type: String = "", // Misal "Manga", "Anime", "Movie"
-    val year: Int = 0 // Tahun rilis
-)
--- app/src/main/java/com/example/nandogami/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nandogami.model
-
-data class User(
-    val username: String = "",
-    val handle: String = "",
-    val bio: String = "",
-    val email: String = "",
-    val photoUrl: String = "" // URL foto profil user
-)
